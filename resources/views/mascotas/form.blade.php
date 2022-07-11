20caa9f0f76f20bd8723ef91ce83e5db0841b819
ZairaHernandez <zs19017433@estudiantes.uv.mx>
2022-07-11 15:09:58 -0500

    Cambio de posición de iconos

--- resources/views/mascotas/form.blade.php
+++ resources/views/mascotas/form.blade.php
@@ -14,44 +14,44 @@
 @endif
 
 <div class="form-group">
-    <label for="Nombre"> Nombre </label>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-vector-pen"
         viewBox="0 0 16 16">
         <path fill-rule="evenodd"
             d="M10.646.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .708l-1.902 1.902-.829 3.313a1.5 1.5 0 0 1-1.024 1.073L1.254 14.746 4.358 4.4A1.5 1.5 0 0 1 5.43 3.377l3.313-.828L10.646.646zm-1.8 2.908-3.173.793a.5.5 0 0 0-.358.342l-2.57 8.565 8.567-2.57a.5.5 0 0 0 .34-.357l.794-3.174-3.6-3.6z" />
         <path fill-rule="evenodd" d="M2.832 13.228 8 9a1 1 0 1 0-1-1l-4.228 5.168-.026.086.086-.026z" />
     </svg>
+    <label for="Nombre"> Nombre </label>
+
     <input type="text" class="form-control" name="Nombre" placeholder="Por ejemplo, Manchitas"
         value="{{ isset($mascotas->Nombre) ? $mascotas->Nombre : old('Nombre') }}" id="Nombre">
     <br>
 </div>
 
 <div class="form-group">
-    <i class="fa-light fa-dog"></i><label for="Tipo"> Tipo Animal </label>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
         class="bi bi-piggy-bank-fill" viewBox="0 0 16 16">
         <path
             d="M7.964 1.527c-2.977 0-5.571 1.704-6.32 4.125h-.55A1 1 0 0 0 .11 6.824l.254 1.46a1.5 1.5 0 0 0 1.478 1.243h.263c.3.513.688.978 1.145 1.382l-.729 2.477a.5.5 0 0 0 .48.641h2a.5.5 0 0 0 .471-.332l.482-1.351c.635.173 1.31.267 2.011.267.707 0 1.388-.095 2.028-.272l.543 1.372a.5.5 0 0 0 .465.316h2a.5.5 0 0 0 .478-.645l-.761-2.506C13.81 9.895 14.5 8.559 14.5 7.069c0-.145-.007-.29-.02-.431.261-.11.508-.266.705-.444.315.306.815.306.815-.417 0 .223-.5.223-.461-.026a.95.95 0 0 0 .09-.255.7.7 0 0 0-.202-.645.58.58 0 0 0-.707-.098.735.735 0 0 0-.375.562c-.024.243.082.48.32.654a2.112 2.112 0 0 1-.259.153c-.534-2.664-3.284-4.595-6.442-4.595Zm7.173 3.876a.565.565 0 0 1-.098.21.704.704 0 0 1-.044-.025c-.146-.09-.157-.175-.152-.223a.236.236 0 0 1 .117-.173c.049-.027.08-.021.113.012a.202.202 0 0 1 .064.199Zm-8.999-.65a.5.5 0 1 1-.276-.96A7.613 7.613 0 0 1 7.964 3.5c.763 0 1.497.11 2.18.315a.5.5 0 1 1-.287.958A6.602 6.602 0 0 0 7.964 4.5c-.64 0-1.255.09-1.826.254ZM5 6.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
     </svg>
+    <i class="fa-light fa-dog"></i><label for="Tipo"> Tipo Animal </label>
     <input type="text" class="form-control" name="Tipo" placeholder="Por ejemplo, Perro"
         value="{{ isset($mascotas->Tipo) ? $mascotas->Tipo : old('Tipo') }}" id="Tipo">
     <br>
 </div>
 
 <div class="form-group">
-    <label for="Edad"> Edad(meses) </label>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
         class="bi bi-hourglass-split" viewBox="0 0 16 16">
         <path
             d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z" />
     </svg>
+    <label for="Edad"> Edad(meses) </label>
     <input type="integer" class="form-control" name="Edad" placeholder="Por ejemplo, 6"
         value="{{ isset($mascotas->Edad) ? $mascotas->Edad : old('Edad') }}" id="Edad">
     <br>
 </div>
 
 <div class="form-group">
-    <label for="Enfermedades"> Enfermedades </label>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bug-fill"
         viewBox="0 0 16 16">
         <path
@@ -59,6 +59,8 @@
         <path
             d="M13 6v1H8.5v8.975A5 5 0 0 0 13 11h.5a.5.5 0 0 1 .5.5v.5a.5.5 0 1 0 1 0v-.5a1.5 1.5 0 0 0-1.5-1.5H13V9h1.5a.5.5 0 0 0 0-1H13V7h.5A1.5 1.5 0 0 0 15 5.5V5a.5.5 0 0 0-1 0v.5a.5.5 0 0 1-.5.5H13zm-5.5 9.975V7H3V6h-.5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 0-1 0v.5A1.5 1.5 0 0 0 2.5 7H3v1H1.5a.5.5 0 0 0 0 1H3v1h-.5A1.5 1.5 0 0 0 1 11.5v.5a.5.5 0 1 0 1 0v-.5a.5.5 0 0 1 .5-.5H3a5 5 0 0 0 4.5 4.975z" />
     </svg>
+    <label for="Enfermedades"> Enfermedades </label>
+
     <input type="text" class="form-control" name="Enfermedades"
         placeholder="Por ejemplo, Viruela, infección estomacal, etc."
         value="{{ isset($mascotas->Enfermedades) ? $mascotas->Enfermedades : old('Enfermedades') }}"
@@ -67,13 +69,14 @@
 </div>
 
 <div class="form-group">
-    <label for="Foto"> Foto </label>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-images"
         viewBox="0 0 16 16">
         <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
         <path
             d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z" />
     </svg>
+    <label for="Foto"> Foto </label>
+
     @if (isset($mascotas->Foto))
         <img class="img-thumbnail img-fluid" src="{{ asset('storage') . '/' . $mascotas->Foto }}" width="100"
             alt="">
